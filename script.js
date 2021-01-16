aee13fdf7e29d47234401baeb3bed07fea89091a
nihalanand24 <nihalanand24@gmail.com>
2021-01-16 14:28:59 -0500

    Added comments and fixed formatting

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const app = {};
 
 app.init = function () {
 
+    // when user submits number, store the value and run a function to parse all the required information from the input
     $('form').on('submit', function (event) {
         event.preventDefault();
         const userInput = $('input').val();
         app.parseCC(userInput);
     })
 }
-// let input = '';
 
 app.parseCC = function (input) {
 
@@ -16,7 +16,7 @@ app.parseCC = function (input) {
     const ccString = input.replace(/\s/g, '');
     // .replace() using RegExp solution from https://www.techiedelight.com/remove-whitespaces-string-javascript/
 
-
+    // store all information that can immediately be parsed in an object
     const cc = {
         string: ccString,
         number: Number(ccString),
@@ -24,22 +24,25 @@ app.parseCC = function (input) {
         first2digits: Number(ccString.substring(0, 2))
     }
 
+    // call a function that takes the cc object and calculates the remaining required information and merge the returned data into the cc object
     $.extend(cc, app.checkCC(cc))
 
+    // send the calculated data to the html document
     $('.cc-icon').html(`${cc.icon}`)
     $('.cc-type').html(`${cc.type}`)
 }
 
-// TODO Hello
-
 app.checkCC = function (ccObject) {
+    
     // Calculate the sum of every alternate digit starting from the last digit by adding the modulus and integer dividing by 100 to remove 2 digits from the end
-
     let checksum1 = 0;
     for (let check1 = ccObject.number; check1 > 0; check1 = Math.floor(check1 / 100)) {
         checksum1 += check1 % 10;
     }
 
+    // Multiply every alternate digit starting from the second last digit by 2 and find the sum of these digits.
+    // Integer divide by 10 to start at the 2nd last digit and follow a similar loop to check1 and multiply by 2
+    // If a digit is greater than 5, add the digits of the product instead of the product itself
     let checksum2 = 0;
     for (let check2 = Math.floor(ccObject.number / 10); check2 > 0; check2 = Math.floor(check2 / 100)) {
         if ((check2 % 10) >= 5) {
@@ -50,10 +53,11 @@ app.checkCC = function (ccObject) {
         }
     }
 
+    // Using the calculated checksums determine if the number passes the luhn check
     const luhn = (((checksum1 + checksum2) % 10) == 0) ? true : false;
 
-    // console.log(ccObject.number, checksum1, checksum2, luhn);
-
+    
+    // use conditional statements to check type of credit card number entered
     const length = ccObject.digits;
     const start = ccObject.first2digits;
 
@@ -75,12 +79,13 @@ app.checkCC = function (ccObject) {
         };
     } else {
         return {
-            type: 'Invalid <br>Number',
+            type: 'Invalid Number',
             icon: '<i class="fas fa-times-circle"></i>'
         }
     }
 }
 
+// Initiate function call on document ready
 $(document).ready(function () {
     app.init();
 });
